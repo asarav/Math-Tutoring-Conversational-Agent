317922cd6f94a82066372b34d0fb43b336dc7a5a
Avinash Saravanan <asarav@umich.edu>
2020-10-10 16:21:42 +0200

    Add all states on dialog flow before exercises

--- MathTutor/src/main/kotlin/furhatos/app/mathtutor/flow/mathtutor.kt
+++ MathTutor/src/main/kotlin/furhatos/app/mathtutor/flow/mathtutor.kt
@@ -57,10 +57,127 @@ val IntroductionState : State = state(Interaction) {
     }
 
     onResponse<Yes> {
-
+        goto(Explanation1)
     }
 
     onResponse<No> {
-        
+        propagate()
+    }
+}
+
+val Explanation1 : State = state(Interaction) {
+    onEntry {
+        furhat.ask("Division is a method of distributing a group of things into equal parts. It is one of the four basic operations of arithmetic, which gives a fair result of sharing."
+        +delay(1000) // Pausing for 2000 ms
+                + "I'll give you an example: Imagine there are are 12 chocolates, and 3 friends want to share them equally, how do they divide the chocolates?"
+        +delay(3000)
+        + "The question is: what is 12 chocolates divided by 3 friends? The answer is: They get 4 each. So 12 divided by 3 is 4.Do you understand this?")
+    }
+
+    onResponse<Yes> {
+        goto(UserUnderstandsAfterFirstExplanation)
+    }
+
+    onResponse<No> {
+        goto(FamiliarWithMultiplication)
+    }
+}
+
+val UserUnderstandsAfterFirstExplanation : State = state(Interaction) {
+    onEntry {
+        furhat.ask("Great! You're a fast learner, "+ users.current.userData.name + ". Do you want me to show you another example?")
+    }
+
+    onResponse<Yes> {
+        goto(Example3)
+    }
+
+    onResponse<No> {
+        goto(ReadyForExercises)
+    }
+}
+
+val FamiliarWithMultiplication : State = state(Interaction) {
+    onEntry {
+        furhat.ask("That is no problem at all! We can take things slowly. I will try to explain it to you in a different way. Are you familiar with multiplication?")
+    }
+
+    onResponse<Yes> {
+        goto(Explanation2)
+    }
+
+    onResponse<No> {
+        goto(Explanation3)
+    }
+}
+
+val Explanation2 : State = state(Interaction) {
+    onEntry {
+        furhat.say("Great! Multiplication is actually the opposite of division, or we can call it the reverse of multiplication."
+                + "In multiplication, we want to know the total of groups of numbers. So if we want to know the total of 2 groups of 4, we multiply 2 by 4 which results into 8."
+                + "In division, we want to divide the total into a few groups and we want to know how many are in each group."
+                + " Imagine that we want to divide 8 into 2 equal groups, we want to know how many there are in each group. So 8 divided by 2 is 4. I'll give you another example.")
+        goto(Example2)
+    }
+}
+
+val Explanation3 : State = state(Interaction) {
+    onEntry {
+        furhat.say("Alright, then I'll explain it this way: "
+                + "In division, we want to divide the total into a few equal groups and we want to know how many are in each group."
+                + " Or we can say: We want to divide the total into groups of a certain number and we want to know how many groups there are.")
+        goto(Example2)
+    }
+}
+
+val Example2 : State = state(Interaction) {
+    onEntry {
+        furhat.say("Imagine that we have a total of 10 students, and we want to divide them into 2 classrooms. How many people will there be in each class room? So, what is 10 divided by 2?"
+                +delay(3000)
+                + "The answer is 5. If we divide 10 students by 2 classrooms, each classroom will have 5 students.")
+
+        random(furhat.ask("Do you understand how division works?"),
+                furhat.ask("Do you see what I mean?"),
+                furhat.ask("Do you understand it?"),
+                furhat.ask("Are you able to follow it?"))
+    }
+
+    onResponse<Yes> {
+        goto(UserUnderstands)
+    }
+
+    onResponse<No> {
+        //Dead End?
+        furhat.say("Don't worry, " + users.current.userData.name + ", it is completely normal if you are struggling to understand it. Don't give up, you will get there! I will take time to explain it to you again.")
+    }
+}
+
+val UserUnderstands : State = state(Interaction) {
+    onEntry {
+        furhat.ask("That's great! Good job, " + users.current.userData.name +". Do you want to see another example")
+    }
+
+    onResponse<Yes> {
+        goto(Example3)
+    }
+
+    onResponse<No> {
+        goto(ReadyForExercises)
+    }
+}
+
+val Example3 : State = state(Interaction) {
+    onEntry {
+        furhat.ask("What is 24 divided by 4?"
+                +delay(3000)
+                +"The question here is: how can we equally divide 24 into 4 groups? The answer is 6. 24 divided by 4 is 6.")
+
+        goto(ReadyForExercises)
+    }
+}
+
+val ReadyForExercises : State = state(Interaction) {
+    onEntry{
+        furhat.say("Alright! Let's jump into some exercises.")
     }
 }
